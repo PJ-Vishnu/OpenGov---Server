cd5b1f8e158b98a67fc2cc8261bc8c8c1d7162b5
PJ-Vishnu <65999631+PJ-Vishnu@users.noreply.github.com>
2024-07-17 19:02:00 +0530

    trying to host

--- db/database.js
+++ db/database.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose"
 
-export const connectMongoDB = () => {
-    mongoose.connect(process.env.MONGODB_URL).then((result) => {
-        console.log(`mongodb connected on host : ${result.connection.host}`);
-        console.log(`mongodb connected databse : ${result.connection.name}`);
-    }).catch((err) => {
-        console.log(err);
-    })
-}
+export const connectMongoDB = async () => {
+    try {
+      const result = await mongoose.connect(process.env.MONGODB_URL);
+      console.log(`mongodb connected on host: ${result.connection.host}`);
+      console.log(`mongodb connected database: ${result.connection.name}`);
+    } catch (err) {
+      console.error('Error connecting to MongoDB:', err);
+    }
+  };
+  
